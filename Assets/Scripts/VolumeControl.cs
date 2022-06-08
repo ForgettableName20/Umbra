03b80e2cd7a4c334b7d7ea59d861a777fe0120b9
Ana Soares <a47500@alunos.ipb.pt>
2022-06-08 12:36:42 +0100

    GameMenu AnimationWorking_Trying to fix the mixers stuff

--- Assets/Scripts/VolumeControl.cs
+++ Assets/Scripts/VolumeControl.cs
@@ -7,46 +7,11 @@ using UnityEngine.UI;
 
 public class VolumeControl : MonoBehaviour
 {
-    [SerializeField] string MasterVol = "MasterVol";
-    [SerializeField] string MusicVol = "MusicVol";
-    [SerializeField] string EffectsVol = "EffectsVol";
+    public AudioMixer audioMixer;
 
-    [SerializeField] float _multiplier = 30f;
-
-    public AudioMixer MainMixer;
-
-    [SerializeField] Slider MasterSlider;
-    [SerializeField] Slider MusicSlider;
-    [SerializeField] Slider EffectsSlider;
-
-    void Start()
+    public void SetMasterVol (float MasterVolume)
     {
-        MasterSlider.value = PlayerPrefs.GetFloat(MasterVol, MasterSlider.value);
-        Audio_Awake();
-    }
-
-    private void Audio_Awake()
-    {
-        MasterSlider.onValueChanged.AddListener(HandleSliderValueChanged);
-    }
-
-    private void OnDisable()
-    {
-        PlayerPrefs.SetFloat(MasterVol, MasterSlider.value);
-    }
-
-    private void HandleSliderValueChanged(float value)
-    {
-        Debug.Log("Slider change");
-    }
-
-    public void ShowSliderValue()
-    {
-        string sliderMessage = "Slider value = " + MasterSlider.value;
-        Debug.Log(sliderMessage);
-       // MainMixer.SetFloat(MasterVol, value: Mathf.Log10(MasterSlider.value) * _multiplier);
-        MainMixer.SetFloat(MasterVol, value: MasterSlider.value);
-
+        audioMixer.SetFloat("MasterVol", MasterVolume);
     }
 
 }
